b541c8f9fc943276579037eb3337d86338898926
dato12T <datoabzianidze12@gmail.com>
2025-03-08 19:58:18 +0400

    commit

---
+++ day04/classwok/lesson04.py
@@ -0,0 +1,9 @@
+name = "dato"
+age = 13 
+name = name = "!"
+print(name)
+age = age + 1
+
+name = "gurami"
+age = "15"
+print("name"  + int(age))
---
+++ day04/homework/lesson04.py
@@ -0,0 +1,14 @@
+x = 4
+y = 10
+z = 16
+number = 7
+str1 = "hello"
+str2 = "teatcher"
+
+a = 7 
+print("old object" + a)
+
+a = 10 
+print("new object" + a)
+
+print("result: " +str(number))
