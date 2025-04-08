d5521a5d2ac024727ec2f1f575dd7ef8e89c6022
dato12T <datoabzianidze12@gmail.com>
2025-04-08 22:12:57 +0400

    commit

---
+++ day06/classwork/lesson1.py
@@ -0,0 +1,7 @@
+#მომხმარებელს შემოატანინეთ inputით თავისი ასაკი და printის საშუალებით, კონსოლში გამოიტანეთ ეს ასაკი
+age = input ("enter your age: ")
+print(age)
+
+#მომხმარებელს შემოატანინეთ თავისი სახელი და printის საშუალებით, კონსოლში გამოიტანეთ "Hello, name"
+name = input ("enter your name: ")
+print("Hello, " + name)
