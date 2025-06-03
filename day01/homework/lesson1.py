8bc8253d238cfd049e2491775efedc4c98910385
datoabzianidze12 <datoabzianidze12@gmail.com>
2025-06-03 17:47:31 +0400

    commit

---
+++ day01/homework/lesson1.py
@@ -0,0 +1,23 @@
+name = "dato"
+
+
+surname = "abzianidze"
+
+print (name) 
+
+age= 12
+height= 160
+
+knows_programming = True
+
+is_ugly = False
+
+print(name + " " + surname)
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
+
+
---
+++ day10/classwork/lesson1.py
@@ -0,0 +1,2 @@
+print(True and False or False and False or True or True or False)
+#and  უფრო ძლიერია ვიდრე or, ამიტომ ჯერ and ვწერთ
