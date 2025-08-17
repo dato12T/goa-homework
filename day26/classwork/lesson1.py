3ece68d8a4a42afa1cb5ea50ee414c7d42f5e7ef
datoabzianidze12 <datoabzianidze12@gmail.com>
2025-08-17 15:36:13 +0400

    commit

---
+++ day26/classwork/lesson1.py
@@ -0,0 +1,16 @@
+
+result = []
+
+for i in range(10):
+    num = int(input(f"შეიყვანეთ {i+1}-ე რიცხვი: "))
+    if num % 2 == 0:  
+        result.append(num)
+
+print("ლუწი რიცხვები:", result)
+
+
+
+names = ["Gurami", "Gio", "Andria", "Mariami", "Dato"]
+
+long_names = [name for name in names if len(name) > 5]
+print("გრძელი სახელები (>5):", long_names)
