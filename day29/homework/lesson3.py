bd9ec78d0cc780ea6e28d0bbdf078679cc664328
datoabzianidze12 <datoabzianidze12@gmail.com>
2025-09-23 16:41:39 +0400

    commit

---
+++ day29/homework/lesson3.py
@@ -0,0 +1,7 @@
+text = "სლაისინგი სასწაულია"
+
+print(text[:9]) 
+
+print(text[10:])   
+
+print(text[::-2])
---
+++ day31/classwork/lesson3.py
@@ -0,0 +1,18 @@
+text = "გამარჯობა მსოფლიო"
+
+print(text.upper())
+
+print("HELLO".lower())
+
+print("hello world".title())
+
+print("   hello   ".strip())   
+
+print(text.replace("მსოფლიო", "ყველას")) 
+
+print(text.find("მ"))
+
+print(text.split())  
+
+words = ["მე", "მიყვარს", "დედა"]
+print(" ".join(words)) 
